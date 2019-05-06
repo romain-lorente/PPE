372cbf66a7930b3bc5fc90c97e5e491ff6eb9887
Opineppes <quentin.legrand.627@gmail.com>
2019-05-06 13:08:25 +0200

    Ajout du type pronom

---
+++ PPE/TypesMots/Pronom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE.TypesMots
+{
+    class Pronom : Mot
+    {
+        public Pronom(int id, string texte, string genre, string nombre) : base(id, texte, genre, nombre) { }
+
+        public Pronom(string texte, string genre, string nombre) : base(texte, genre, nombre) { }
+
+
+    }
+}
