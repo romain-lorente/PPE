b48878dcccfe91477788228738a88dd987a0e116
Xyro64 <romain.lorente@epsi.fr>
2019-05-23 14:50:08 +0200

    Ajout de tests pour les mots

---
+++ Test_PPE/Test_Mot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PPE
+{
+    [TestClass]
+    public class Test_Mot
+    {
+        [TestMethod]
+        public void SelectByText_UnMot()
+        {
+            PPEDataBase.Connexion();
+
+            List<Mot> res = PPEDataBase.Mot.SelectByText("Quentin");
+            Assert.AreEqual(1, res.ToArray().Length);
+        }
+
+        [TestMethod]
+        public void SelectByText_DeuxMots()
+        {
+            PPEDataBase.Connexion();
+
+            List<Mot> res = PPEDataBase.Mot.SelectByText("marche");
+            Assert.AreEqual(2, res.ToArray().Length);
+        }
+    }
+}
